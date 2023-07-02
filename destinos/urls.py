405831d6ff7805dde4251327c8ebfe97dde4b3d3
Jean <jcharaco@unsa.edu.pe>
2023-07-01 23:34:12 -0500

    avance creacion de formulario para añadir destinos

--- destinos/urls.py
+++ destinos/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns = [
     path('', views.destinos_turisticos, name='destinos_turisticos'),
+    path('crear_destinos/', views.crear_destinos, name='crear_destinos'),
     path('destino/<int:destino_id>/', views.detalle_destino, name='detalle_destino'),
 ]
