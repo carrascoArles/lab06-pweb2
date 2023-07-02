ffd9862607ddee8799585db94852ccfe93bbefcd
carrascoArles <acarrascoch@unsa.edu.pe>
2023-07-01 19:05:44 -0500

    actualizado

--- lugares_turisticos/settings.py
+++ lugares_turisticos/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 # Application definition
 
 INSTALLED_APPS = [
-
+    'destinos.apps.DestinosConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -121,11 +121,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
 ]
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- lugares_turisticos/urls.py
+++ lugares_turisticos/urls.py
@@ -17,11 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-from cuentas import views
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.index, name='destinos_turisticos'),
+    path('', include('destinos.urls')),
     path('admin/', admin.site.urls),
     path('cuentas/',include('cuentas.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)   
